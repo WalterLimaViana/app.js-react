6a6ff18137094ad4b6128de8f42a8158103b8fa3
WalterLimaViana <walter.lima.viana@gmail.com>
2021-07-18 20:59:35 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Item from "./components/Item";
+import Card from "./components/Card";
+
+const App = () => {
+  return(
+    <>
+      <h1>Minha primeira aplicação com react</h1>
+      <h2>Lista de Itens</h2>
+      <ul>
+        <Item>
+        Item 1  
+        </Item>       
+        <Item>
+        Item 2  
+        </Item>
+        <Item>
+        Item 3  
+        </Item>     
+      </ul>
+      <Card/>
+    </>
+    
+  )
+}
+
+export default App;
+
